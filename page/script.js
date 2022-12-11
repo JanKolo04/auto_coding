72d1704ad8574b4eaf4c2ebc4c522f6b9c14a579
JanKolo04 <jankolodziej04@gmail.com>
2022-12-11 21:12:47 +0100

    code

---
+++ page/script.js
@@ -0,0 +1,11 @@
+function scratch_data() {
+    let input_val = document.querySelector("#command").value;
+
+    //split data from input
+    let split_input_val = input_val.split(" ");
+    
+    return split_input_val;
+}
+
+
+
